533b7a77cf7ee58303051d9cebf9c472923fe238
ashwinhubli <66745007+ashwinhubli@users.noreply.github.com>
2020-12-17 19:37:18 +0530

    Add files via upload

---
+++ screens/SettingsScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Settings } from 'react-native';
+import {View,Text} from 'react-native';
+import { exp } from 'react-native-reanimated';
+
+export default class SettingsScreen extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>Settings</Text>
+            </View>
+        )
+    }
+}
